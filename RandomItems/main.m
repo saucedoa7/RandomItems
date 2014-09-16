d1a77c1e0ad69b0f860928b0ef9ebe98107d18cc
Albert Saucedo <saucedoa7@gmail.com>
2014-09-15 22:31:16 -0500

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Albert Saucedo on 9/15/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
